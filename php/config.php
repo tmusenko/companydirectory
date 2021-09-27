c166e50c1def8aabf269ce487b6dc0d33f1f341d
tmusenko <66254894+tmusenko@users.noreply.github.com>
2021-09-27 23:10:30 +0100

    Add files via upload

---
+++ php/config.php
@@ -0,0 +1,10 @@
+<?php
+
+	$cd_host = "localhost";
+	$cd_port = 3306;
+	$cd_socket = "";
+	$cd_user = "invata"; // user name
+	$cd_password = "Ingrida1995!"; // password
+	$cd_dbname = "companydirectory"; // database name
+
+?>
